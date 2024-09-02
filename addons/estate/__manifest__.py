53dcc8db4f3787e826ad745cbcc13788a0867f1f
kulidaden <kulidadinis@gmail.com>
2024-09-02 11:21:29 +0300

    1 comm - push all

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Estate",
+    'depends': ['base'],
+    'version':'1.0',
+    'category': 'Uncategorized',
+    'application': True,
+    'data': [
+        'seq/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        'views/estate_property_search.xml'
+            ],
+}
---
+++ addons/estate/models/estate_property.py
@@ -0,0 +1,39 @@
+from odoo import fields,models
+from datetime import datetime, timedelta
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description='This module created by Kulida Denys'
+
+    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
+    name = fields.Char(string="Estate", required=True, default='Unknown')
+    date_availability = fields.Date(string="Availability Date",
+                                    default=lambda self: datetime.today() + timedelta(days=90),copy=False)
+    postcode = fields.Char(string="Postcode")
+    expected_price = fields.Float(string="Expected Price", required=True)
+    garage = fields.Boolean(string="Garage")
+    selling_price = fields.Float(string="Selling Price",readonly=True)
+    garden = fields.Boolean(string="Garden")
+    bedrooms = fields.Integer(string="Bedrooms")
+    active=fields.Boolean(string='Active',default=True)
+    living_area = fields.Integer(string="Living Area (sqm)")
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', "North"),
+            ('south', "South"),
+            ('east', "East"),
+            ('west', "West")
+        ],
+    )
+    facades = fields.Integer(string="Facades")
+    state=fields.Selection(string='State',required=True,copy=False,default="new",
+                           selection=[
+                               ('new',"New"),
+                               ('offer received',"Offer Received"),
+                               ('offer accepted',"Offer Accepted"),
+                               ('sold',"Sold"),
+                               ('canceled',"Canceled")
+                           ]
+    )
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    description = fields.Text(string="Description")

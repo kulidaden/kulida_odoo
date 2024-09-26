4e9a9446a63499598f639550bfcc101eb88c5267
kulidaden <kulidadinis@gmail.com>
2024-09-26 09:29:04 +0300

    перейменування

--- addons/hospital/__manifest__.py
+++ addons/hospital/__manifest__.py
@@ -6,7 +6,7 @@
     'application': True,
     'data':[
         'security/ir.model.access.csv',
-        'wizard/assign_doctor_wizard.xml',
+        'wizard/doctor_wizard.xml',
         'view/hospital_view.xml',
         'view/hospital_menu.xml'
 
--- addons/hospital/wizard/__init__.py
+++ addons/hospital/wizard/__init__.py
@@ -1,1 +1,1 @@
-from . import assign_doctor
+from . import wizard
